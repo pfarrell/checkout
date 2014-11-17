ea5681f68aeda7a21f52b40d8de5f9e0b1a4bbec
Patrick Farrell <mr.patfarrell@gmail.com>
2014-11-16 23:12:45 -0800

    simple reporting

---
+++ app/routes/report.rb
@@ -0,0 +1,33 @@
+class App < Sinatra::Application
+  get '/reports' do
+    haml :reports
+  end
+
+  get '/reports/items' do
+    props={}
+    props["Item ID"]= lambda{|x| x.item.item_id} 
+    props["Item Description"] = lambda{|x| x.item.name} 
+    props["Purchaser"]= lambda{|x| x.bidder.name} 
+    props["Price"]= lambda{|x| x.purchase_price} 
+
+    data=Purchase.all
+    total=data.inject(0.0){|sum,p| sum + p.price}
+
+    haml :report, locals:{header: props, data: data, total:total }
+  end
+
+  get '/reports/bidders' do
+    props={}
+    props["Bidder ID"]= lambda{|x| x.bidder_id} 
+    props["Bidder "] = lambda{|x| x.name} 
+    props["Purchases"]= lambda{|x| x.purchases.size} 
+    props["Total Price"]= lambda{|x| '%0.2f' % x.purchases.inject(0.0){|sum,p| sum + p.price}} 
+
+    data=Bidder.all
+    total='%0.2f' % data.inject(0.0){|sum,b| sum + b.purchases.inject(0.0){|sum2,p| sum2 + p.price}}
+
+    haml :report, locals:{header: props, data: data, total:total }
+  end
+
+end
+
